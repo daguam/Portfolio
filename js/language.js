237f301966c49a4bfc4252ede29b3f2837e9491f
daguam <daguam93@gmail.com>
2024-07-23 15:08:02 -0600

    Add Landing Page updated files

--- js/language.js
+++ js/language.js
@@ -2,12 +2,14 @@ const translations = {
     en: {
         name: "David Gutiérrez",
         cvButton: "View My CV",
-        footerText: "&copy; 2024 David Gutiérrez. All rights reserved."
+        footerText: "&copy; 2024 David Gutiérrez. All rights reserved.",
+        comingSoon: "Coming Soon"
     },
     es: {
         name: "David Gutiérrez",
         cvButton: "Ver Mi CV",
-        footerText: "&copy; 2024 David Gutiérrez. Todos los derechos reservados."
+        footerText: "&copy; 2024 David Gutiérrez. Todos los derechos reservados.",
+        comingSoon: "Próximamente"
     }
 };
 
@@ -16,4 +18,5 @@ function switchLanguage() {
     document.getElementById('name').innerText = translations[language].name;
     document.getElementById('cvButton').innerText = translations[language].cvButton;
     document.getElementById('footerText').innerHTML = translations[language].footerText;
+    document.querySelector('.coming-soon-badge').innerText = translations[language].comingSoon;
 }
